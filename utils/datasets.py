54702aef5acb6ac7d20c5bc9aa6afee5a2961b06
unknown <yihsiang.fang@gmail.com>
2022-03-08 01:51:32 +0100

    update dataset.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -92,7 +92,7 @@ def exif_transpose(image):
 
 
 def create_dataloader(path, imgsz, batch_size, stride, single_cls=False, hyp=None, augment=False, cache=False, pad=0.0,
-                      rect=False, rank=-1, workers=8, image_weights=False, quad=False, prefix='', shuffle=False, training=True):
+                      rect=False, rank=-1, workers=8, image_weights=False, quad=False, prefix='', shuffle=False, training=True, subset=[]):
     if rect and shuffle:
         LOGGER.warning('WARNING: --rect is incompatible with DataLoader shuffle, setting shuffle=False')
         shuffle = False
@@ -107,7 +107,8 @@ def create_dataloader(path, imgsz, batch_size, stride, single_cls=False, hyp=Non
                                       pad=pad,
                                       image_weights=image_weights,
                                       prefix=prefix,
-                                      training=training)
+                                      training=training,
+                                      subset=subset)
 
     batch_size = min(batch_size, len(dataset))
     nd = torch.cuda.device_count()  # number of CUDA devices
@@ -441,6 +442,7 @@ class LoadImagesAndLabels(Dataset):
         #subset = [0,2,3,5,11,12,15,16]
         #subset = [0,2,4,6,8,10,12,14,16,18,20]
         #subset = list(range(16))
+        print("subset: ", len(subset))
 
         if training == True: 
             print("In train phase")
